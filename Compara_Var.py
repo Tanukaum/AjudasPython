ba2f43013d084ec3427dd9ca062e838676587e92
Isabella <guilabela10@gmail.com>
2022-09-05 11:59:58 -0300

    Compara Variavéis == e !=

---
+++ Compara_Var.py
@@ -0,0 +1,23 @@
+a=0
+b=0
+c=0
+
+if a==b==c==0:
+    print(a)
+    print(b)
+    print(c)
+    print('São iguais a 0, a=b=c = 10')
+    a=b=c = 10
+    print(a)
+    print(b)
+    print(c)
+    b=5
+    c=15
+
+if a != b != c:
+    print(a)
+    print(b)
+    print(c)
+    x = a+b+c
+    print(x)
+
