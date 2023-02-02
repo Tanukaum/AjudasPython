5535526d4afab8cd9656b2cd4112306dca6363bc
IsaKelles <isabella.kelles.-nd@disney.com>
2023-02-02 11:39:56 -0300

    Concat_DropDuplicate

---
+++ Pandas/Concat_DropDuplicates.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+id = date = time= local = ['a','a_1']
+
+id_2 = date_2 = time_2 = local_2 = ['a','b','c']
+
+df = pd.DataFrame({'Id': id, 'Date': date, 'Hour': time, 'PGM/PVW': local})
+print('Creating excel')
+
+df_2 = pd.DataFrame({'Id': id_2, 'Date': date_2, 'Hour': time_2, 'PGM/PVW': local_2})   
+
+df = pd.concat([df, df_2])
+new_df = df.drop_duplicates(keep='first')
+print(new_df)
